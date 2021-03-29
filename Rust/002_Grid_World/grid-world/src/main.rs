5f92b3c16cfc8a9ad3670117091c89417eb9c783
marvin <marvin@thismarvin.com>
2021-03-29 13:48:11 -0500

    bugfix: grid is no longer cut off

--- Rust/002_Grid_World/grid-world/src/main.rs
+++ Rust/002_Grid_World/grid-world/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut config = AppConfig::new();
 
     config.title = "Grid World";
-    config.window_size = (640, 360);
+    config.window_size = (150 * 4, 150 * 3);
     config.vsync_enabled = true;
     config.core = Some(Box::new(Game::new(std::env::args())));
 
